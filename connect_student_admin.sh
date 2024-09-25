15e761bb253f08aab652bc571f536c2019fdad86
jakewatson91 <155555254+jakewatson91@users.noreply.github.com>
2024-09-25 15:02:46 -0400

    Update connect_student_admin.sh
    student-admin-key path

--- connect_student_admin.sh
+++ connect_student_admin.sh
@@ -3,4 +3,4 @@
 PORT=22002
 MACHINE=paffenroth-23.dyn.wpi.edu
 
-ssh -i ../../scripts/student-admin_key -p ${PORT} -o StrictHostKeyChecking=no student-admin@${MACHINE}
+ssh -i $HOME/.ssh/student-admin_key -p ${PORT} -o StrictHostKeyChecking=no student-admin@${MACHINE}
