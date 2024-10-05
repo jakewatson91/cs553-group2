505e816ceab7c1d3ce991378b9f6e7f08254a641
Andrew Keane <50113650+1keane@users.noreply.github.com>
2024-10-05 19:33:20 -0400

    Put dates in echos for timing.

--- mlops_product.sh
+++ mlops_product.sh
@@ -27,29 +27,29 @@ cd ~/git
 rm -rf $MLOPSPRODUCT
 
 #-- Clone the GitHub Repo with Group 2 Product
-echo ; echo "Cloning GitHub repo - $MLOPSPRODUCT"
+echo ; echo "`date` - Cloning GitHub repo - $MLOPSPRODUCT"
 git clone git@github.com:jakewatson91/cs553-group2
 
 #-- Copy the product files to the server
-echo ; echo "Copying proudct code to server"
+echo ; echo "`date` - Copying proudct code to server"
 scp -p -i $MLOPSPERSKEY -P $MLOPSPORT -r $MLOPSPRODUCT $MLOPSADMIN@$MLOPSHOST:
 
 #-- check that the code in installed and start up the product
-echo ; echo "Verifying product exists on server"
+echo ; echo "`date` - Verifying product exists on server"
 SSHCMD="ssh -i $MLOPSPERSKEY -p $MLOPSPORT $MLOPSADMIN@$MLOPSHOST"
 $SSHCMD "ls -lad cs553-group2"
 
 #-- Install the Python virtual environment application
-echo ; echo "Installing Pything virtual environment application"
+echo ; echo "`date` - Installing Pything virtual environment application"
 $SSHCMD "sudo apt install -qq -y python3-venv"
 
 #-- Create and activate the Python virtual environment
-echo ; echo "Create and activate the Python virtual environment, install required packages"
+echo ; echo "`date` - Create and activate the Python virtual environment, install required packages"
 $SSHCMD "cd cs553-group2 && python3 -m venv venv"
 $SSHCMD "cd cs553-group2 && source venv/bin/activate && pip install -r requirements.txt"
 
 #-- Start the product and use nohup to keep up even if terminal windows close
-echo ; echo "Start the product"
+echo ; echo "`date` - Start the product"
 # $SSHCMD "nohup cs553-group2/venv/bin/python3 CS553_example/app.py > log.txt 2>&1 &"
 
 
